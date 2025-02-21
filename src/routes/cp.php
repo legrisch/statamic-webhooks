0d3c1064f2df6e38f6b208778ee5fb607f7b5063
Grischa Erbe <grischa.erbe@googlemail.com>
2025-02-21 01:06:03 +0100

    laravel 8 update

--- src/routes/cp.php
+++ src/routes/cp.php
@@ -1,8 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Legrisch\StatamicWebhooks\Http\Controllers\SettingsController;
 
 Route::middleware('web')->group(function () {
-  Route::get('/legrisch/statamic-webhooks', 'SettingsController@index')->name('legrisch.statamic-webhooks.index');
-  Route::post('/legrisch/statamic-webhooks', 'SettingsController@update')->name('legrisch.statamic-webhooks.update');
+  Route::get('/legrisch/statamic-webhooks', [SettingsController::class, 'index'])->name('legrisch.statamic-webhooks.index');
+  Route::post('/legrisch/statamic-webhooks', [SettingsController::class, 'update'])->name('legrisch.statamic-webhooks.update');
 });
